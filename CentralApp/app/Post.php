14b9fde46b532b83fd3d06911b50805e09570800
mainul-alam <mainulkst@gmail.com>
2020-08-10 16:52:37 +0200

    after all project creation

--- CentralApp/app/Post.php
+++ CentralApp/app/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    // Table name 
+    protected $table ='posts';
+    // primary key 
+    public $primarykey ='id';
     //
+    public $timestamps = true;
+
 }
